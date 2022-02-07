dafcca4193a61201c10fb090f72548d522df2164
Ming-der Wnag <mingderwang@gmail.com>
2022-02-07 23:03:27 +0800

    add instagram, but got 400 instead of 200

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,52 @@ import NextAuth from "next-auth";
 
 export default NextAuth({
   providers: [
+      {
+      id: "instagram",
+      name: "instagram",
+      type: "oauth",
+      version: "2.0",
+      token: "https://api.instagram.com/oauth/access_token",
+      authorization: {
+        url: "https://api.instagram.com/oauth/authorize",
+        params: {
+          scope: "user_profile",
+        },
+      },
+      userinfo: {
+        url: "https://graph.instagram.com/me?fields=id",
+        async request({ client, tokens }) {
+          // Get base profile
+          console.log("client", client);
+          console.log("tokens", tokens);
+          const profile = await client.userinfo(tokens);
+          // no email info from Pinterest API
+          if (!profile.email) {
+            profile.email = profile.name;
+          }
+          return profile;
+        },
+      },
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+
+      profile(profile, accessToken) {
+        console.log('token', accessToken);
+        console.log('profile', profile);
+        return {
+          id: profile.username,
+          name: profile.username,
+          email: profile.email,
+          image: profile.profile_image,
+        };
+      },
+      checks: "none",
+      headers: {},
+      authorizationParams: {
+        client_id: process.env.INSTAGRAM_CLIENT_ID,
+        redirect_uri: encodeURIComponent(process.env.INSTAGRAM_REDIRECT_URI),
+      },
+    },
     {
       id: "pinterest",
       name: "Pinterest",
@@ -17,7 +63,6 @@ export default NextAuth({
             "ads:read,boards:read,boards:read_secret,boards:write,boards:write_secret,pins:read,pins:read_secret,pins:write,pins:write_secret,user_accounts:read",
         },
       },
-      token: "https://api.pinterest.com/v5/oauth/token",
       userinfo: {
         url: "https://api.pinterest.com/v5/user_account",
         async request({ client, tokens }) {
